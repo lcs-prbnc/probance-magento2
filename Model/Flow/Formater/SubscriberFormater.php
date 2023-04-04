2f0df8ba3de312dc9a613bb71ddb7b332e37a7de
Gérald CANN <sdkweb.projects@gmail.com>
2023-04-04 19:25:54 +0200

    corretc subscriber with H:i:s too

--- Model/Flow/Formater/SubscriberFormater.php
+++ Model/Flow/Formater/SubscriberFormater.php
@@ -28,7 +28,7 @@ class SubscriberFormater extends AbstractFormater
     public function getCreatedAt(Subscriber $subscriber)
     {
         $datetime = new \DateTime($subscriber->getChangeStatusAt());
-        return $datetime->format('Y-m-d');
+        return $datetime->format('Y-m-d H:i:s');
     }
 
     /**
